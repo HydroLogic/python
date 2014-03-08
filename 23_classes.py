e6dd3a455c6b9f1ba4e09321f4a793b7daa8329e
minorg <minorg@server.fake>
2014-03-08 11:27:00 -0600

    cleaned up classes

--- 23_classes.py
+++ 23_classes.py
@@ -1,25 +1,25 @@
 # A very brief overview
 
-class SomeClass(object): # Inherit the root class, object
+class Person(object): # Inherit the root class, object
     # Method = function declared within a class block
     # First positional parameter is always the instance, traditionally named "self" (vs. "this" in Java or C++)
-    def __init__(self, someparam):
-        self.CONSTANT = 3.14
-        self.public = 'test'
-        self.someparam = someparam
+    def __init__(self, first_name, last_name):
+        self.SPECIES = "Homo sapiens sapiens"
+        self.first_name = first_name
+        self.last_name = last_name
 
-    def get_someparam_plus_1(self):
-        return self.someparam + 1
+    def get_name(self):
+        return self.first_name + ' ' + self.last_name
 
-x = SomeClass(1) # Create/"instantiate" SomeClass
-                 # Implicitly calls the methods __init__(x, 1)
-# x is an "instance" of SomeClass
-x.get_someparam_plus_1() # Conceptually like calling SomeClass.get_someparam_plus_1(x)
+me = Person('Minor', 'Gordon') # Create/"instantiate" Person
+                               # Implicitly calls the methods __init__(me, 1)
+# me is an "instance" of Person
+print me.get_name() # ~ calling Person.get_name(me)
 # The dot syntax just means "access a method or a data member on an instance"
-x.CONSTANT # Not a method, just a variable attached to the instance
-x.public
+print me.first_name
+print me.SPECIES # Not a method, just a variable attached to the instance
 
 # So when you see 'class datetime' here:
 # http://docs.python.org/2/library/datetime
 # Calling datetime.now() creates an instance of the datetime class
-# Then you access it with some_datetime.year, etc., just like x. above
+# Then you access it with some_datetime.year, etc., just like me. above
