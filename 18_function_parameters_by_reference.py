406008c31fe90cb5f22d3ae8ca903c814eda4e33
minorg <minorg@server.fake>
2014-03-08 12:18:18 -0600

    fixed comment in function_parameters_by_reference

--- 18_function_parameters_by_reference.py
+++ 18_function_parameters_by_reference.py
@@ -1,4 +1,4 @@
-# Functions allow for code reuse ("Don't Repeat Yourself")
+# Functions allow for code reuse (DRY, "Don't Repeat Yourself")
 def change_list(a_list): # a_list is a reference, not a copy
     del a_list[0]
     a_list.append('test')
@@ -7,5 +7,5 @@ def change_list(a_list): # a_list is a reference, not a copy
 some_list = ['a', 'b']
 change_list(some_list) # some_list "becomes" a_list in change_list
 some_other_list = ['b', 'c']
-return_value = change_list(some_other_list) # some_other_list "becomes" some_other_list in change_list
+return_value = change_list(some_other_list) # some_other_list "becomes" a_list in change_list
 assert return_value == None
