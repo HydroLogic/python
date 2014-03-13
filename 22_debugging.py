2993bcd6e195fd47db4f6c5d438d5ee576d86fa5
minorg <minorg@server.fake>
2014-03-13 14:33:01 -0500

    tweaked debugging

--- 22_debugging.py
+++ 22_debugging.py
@@ -1,11 +1,9 @@
-import random
-
 def f(a_list):
-    element_i = random.randint(0, len(a_list))
-    #print element_i
-    element = a_list[element_i]
-    #print element_i, element
+    print "Length of list", len(a_list)
+    for element_i in xrange(0, len(a_list)+1):
+        #print element_i
+        element = a_list[element_i]
+        print element_i, element
 
 a_list = 'ya see i go by the code of the doctor of the mix'.split()
-for i in xrange(100):
-    f(a_list)
+f(a_list)
