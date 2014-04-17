6dfc4be3c706b5c6190270971fd19317cb484a7e
Minor Gordon <github@minorgordon.net>
2014-04-17 15:58:46 -0500

    string join tricks

--- 28_string_join.py
+++ 28_string_join.py
@@ -11,3 +11,12 @@ print ', '.join(('Hello', 'world')) # Tuple instead of a list
 # Won't work, because range(10) produces a list of numbers, not strings
 # print ','.join(range(10))
 print ','.join(str(i) for i in xrange(10)) # List comprehension
+
+# Capitalize every word in a sentence
+sentence = 'this is an uncapitalized sentence'
+print ' '.join(word.capitalize() for word in sentence.split())
+
+# Capitalize every other letter
+word = 'longword'
+print ''.join((c.upper()) if i % 2 == 0 else c
+               for i, c in enumerate(word))
