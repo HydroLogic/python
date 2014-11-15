35f256e94b9ea92bdada01735e95f5e043d31e3e
Minor Gordon <github@minorgordon.net>
2014-11-15 16:48:39 -0600

    renumber

--- idioms/09_introspection.py
+++ idioms/09_introspection.py
@@ -1,5 +1,5 @@
 i = 1
-print 'Integer class repr', i.__class__
+print 'Integer class', i.__class__
 print 'Integer class name', i.__class__.__name__
 print 'Integer class module', i.__class__.__module__
 class MyType(object):
@@ -8,4 +8,6 @@ print 'MyType module', MyType().__class__.__module__
 print 'MyType module name (not the module object!)', MyType.__module__
 import sys
 print 'MyType file', sys.modules[MyType.__module__]
-print 'MyType methods', dir(MyType())
+print 'MyType methods:'
+for method_name in dir(MyType()):
+    print method_name
