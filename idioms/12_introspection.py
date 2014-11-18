de61ad75b6fe29567e041bc0fb3e81fa6eda99fa
Minor Gordon <github@minorgordon.net>
2014-11-18 12:41:40 -0600

    expanded introspection module

--- idioms/12_introspection.py
+++ idioms/12_introspection.py
@@ -1,3 +1,5 @@
+import sys
+
 i = 1
 print 'Integer class', i.__class__
 print 'Integer class name', i.__class__.__name__
@@ -9,8 +11,16 @@ class MyType(object):
     pass
 print 'MyType module', MyType().__class__.__module__
 print 'MyType module name (not the module object!)', MyType.__module__
-import sys
 print 'MyType file', sys.modules[MyType.__module__]
 print 'MyType methods:'
 for method_name in dir(MyType()):
     print method_name
+print
+print 'MyType __dict__:',
+inst = MyType()
+inst.x = 1 # Can assign arbitrary attributes. Where do they go?
+print inst.__dict__ # Instances are backed by dicts.
+inst.__dict__['y'] = 2
+print ["%s=%s" % (attr, getattr(inst, attr))
+       for attr in dir(inst)
+       if attr[0] != '_']
